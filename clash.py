a6a1fd9a2e248aa6dcb83581abd929a8480be6dc
redleader36 <redleader36@gmail.com>
2016-10-10 15:36:54 -0500

    Update clash.py

--- clash.py
+++ clash.py
@@ -13,7 +13,7 @@ def _initialize(bot):
     api_key = bot.get_config_option('clash_api_key')
     if api_key:
         _internal['clash_api_key'] = api_key
-        plugins.register_user_command(['clan', 'clanlink'])
+        plugins.register_user_command(['clan', 'clanlink', 'clancompare'])
         plugins.register_admin_command(['setclan'])
     else:
         logger.error('CLASH: config["clash_api_key"] required')
@@ -53,10 +53,18 @@ def clan(bot, event, *args):
         if search.startswith('#'):
             """search by tag"""
             r = coc.clans(search).get()
-            print(r)
+            if r=={'reason': 'notFound'}:
+                yield from bot.coro_send_message(event.conv_id, 'No results found.')
+                return
+
         else:
             """search by name"""
-            r = coc.clans(name=search,limit=1).get()[0]
+            r = coc.clans(name=search,limit=1).get()
+            if r==[]:
+                yield from bot.coro_send_message(event.conv_id, 'No results found.')
+                return                
+            else:
+                r = r[0]
 
     _internal['last_clan'], _internal['last_tag'] = r['name'], r['tag']
     wars = 0
@@ -96,5 +104,43 @@ def clan(bot, event, *args):
         yield from bot.coro_send_message(event.conv_id, 'There was an error retrieving the Clan')
 
 def clanlink(bot, event, *args):
-    url = 'https://www.clashofstats.com/clans/%s/members' % _internal['last_tag'].replace('#', '')
+    """Returns a Clash-of-Stats link to the last-searched Clan
+    <b>/bot clanlink</b>
+    """
+    if _internal.get('last_tag', False):
+        tag = _internal['last_tag']
+    elif bot.memory.exists(["conv_data", event.conv.id_]) and (bot.memory.exists(["conv_data", event.conv.id_, "default_clan"])):
+            tag = bot.memory.get_by_path(["conv_data", event.conv.id_, "default_clan"])
+    else:
+        yield from bot.coro_send_message(event.conv_id, 'There is no criteria retrieving the link.  Try the Clan command to get a Clan tag in memory.')
+    url = 'https://www.clashofstats.com/clans/%s/members' % tag.replace('#', '')
+    yield from bot.coro_send_message(event.conv_id, url)
+
+def clancompare(bot, event, *args):
+    """Returns a link to a Clash-of-Stats comparison page
+    <b>/bot clancompare</b> Returns link to compare Default Clan to the Clan last returned by the <i>/bot clan</i> command.
+    <b>/bot clancompare <clan tag(s)></b> Returns link to compare Default Clan to one of more specified Clans.  Tags must include '#' and must be seperated by a space.
+    """
+    if bot.memory.exists(["conv_data", event.conv.id_]) and (bot.memory.exists(["conv_data", event.conv.id_, "default_clan"])):
+        tag1 = bot.memory.get_by_path(["conv_data", event.conv.id_, "default_clan"])
+    else:
+        yield from bot.coro_send_message(event.conv_id, 'There is no Default clan set. Please ask Admin to set a default')
+        return
+    if not args:
+        if _internal.get('last_tag', False):
+            tag2 = _internal['last_tag']
+        else:
+            yield from bot.coro_send_message(event.conv_id, 'No Clan specified and no Clan in memory.')
+            return
+        if tag2 == tag1:
+            yield from bot.coro_send_message(event.conv_id, 'It\'s pointless to compare the same Clan to itself.')
+            return
+    else:
+        if args[0].startswith('#'):
+            tag2 = ','.join(args)
+        else:
+            yield from bot.coro_send_message(event.conv_id, 'Incorrect Clan tag parameter')
+            return
+        
+    url = 'https://www.clashofstats.com/clans-comparison?clans=%s,%s' % (tag1.replace('#', ''), tag2.replace('#', ''))
     yield from bot.coro_send_message(event.conv_id, url)
